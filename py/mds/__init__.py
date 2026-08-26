81d8167f99d1bf9606ed841cf88a62bafd9355ab
sommelo1 <59441867+sommelo1@users.noreply.github.com>
2026-08-26 07:37:17 +0200

    Release 0.15.2: update package descriptions

--- py/mds/__init__.py
+++ py/mds/__init__.py
@@ -29,7 +29,7 @@ from .validate import (
     validate_streams,
 )
 
-__version__ = "0.15.1"
+__version__ = "0.15.2"
 
 __all__ = [
     "CODES", "Diagnostic", "SEVERITY_ERROR", "SEVERITY_INFO", "SEVERITY_WARNING",
